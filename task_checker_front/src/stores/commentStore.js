ea98bb7ead1d7e5836212cd20fea8024b41f35d8
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-04-13 09:04:29 +0900

    comment Storeの作成

---
+++ task_checker_front/src/stores/commentStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import api from '../api/axios'
+import { ref } from 'vue'
+
+export const useCommentStore = defineStore('comment', () => {
+  const comments = ref([]);
+
+  async function addComment (newComment) {
+    try{
+      const response = await api.post(`/comment`, newComment);
+      const addedComment = response.data;
+      comments.value.push(addedComment);
+    }catch(error){
+      console.log('コメントの保存ができませんでした', error);
+    }
+  }
+
+  return { comments, addComment }
+})
